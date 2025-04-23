cf2d9982249edc2b480ea701bfd0ec14b106602a
Felix Obioma Nkwuzor <felixobioma99@gmail.com>
2025-04-23 19:44:48 +0100

    Add files via upload

---
+++ data.py
@@ -0,0 +1,80 @@
+import requests
+import pandas as pd
+import polars as pl
+from sqlalchemy import create_engine, text,URL 
+import numpy as np
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class stock_data_api:
+    #setup init variables
+    def __init__(self, ticker, api_key=os.environ.get('api_key')):
+        self.ticker= ticker
+        self.api_key= api_key
+    #load data from api
+    def get_data_from_api(self):
+        #url
+        url= f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={self.ticker}&outputsize=full&apikey={self.api_key}'
+        #send get request
+        r= requests.get(url)
+        #get the json data
+        data= r.json()
+        #convert to pandas df
+        df= pd.DataFrame().from_dict(data['Time Series (Daily)'], orient='index')
+        #set index col name to 'date'
+        df.index.name= 'Date'
+        #ensure column names are of approriate format
+        df.columns= [col[3:].capitalize() for col in df.columns.to_list()]
+        #convert to polars df
+        df= pl.from_pandas(df, include_index=True)
+        #convert columns to their appropriate data types
+        df= df.with_columns(
+            pl.col('Date').cast(pl.Date),
+            pl.col('Open').cast(pl.Float64),
+            pl.col('High').cast(pl.Float64),
+            pl.col('Low').cast(pl.Float64),
+            pl.col('Close').cast(pl.Float64),
+            pl.col('Volume').cast(pl.Float64)
+        )
+        return df
+            
+class Db_Repo:
+    #setup init values
+    def __init__(
+        self,
+        url_object= URL.create(
+    "postgresql+psycopg",
+    username=f"{os.environ.get('db_username')}",
+    password=f"{os.environ.get('db_password')}",
+    host=f"{os.environ.get('db_host')}",
+    port=f"{os.environ.get('db_port')}",
+    database=f"{os.environ.get('db_name')}"
+)
+    ):
+        self.url_object= url_object
+
+    #load data into postgres database
+    def insert_data(self, table_name, records):
+        #create sqlalchemy engine
+        engine= create_engine(self.url_object)
+        #setup connection to database
+        with engine.connect() as conn:
+            n_transactions = records.write_database(table_name=table_name, connection=conn, if_table_exists='replace')
+        return n_transactions
+
+    #read data from database
+    def read_data(self, table_name):
+        #create sqlalchemy engine
+        engine= create_engine(self.url_object)
+        #sql query to read data from database
+        query = f"""
+        SELECT * FROM "{table_name}"
+        """
+        #setup connection to database
+        with engine.connect() as conn:
+            df = pl.read_database(query, connection=conn)
+        return df
+
+
+        
---
+++ pages/3.Backtester.py
@@ -0,0 +1,117 @@
+import dash
+from dash import Dash, html, dash_table, dcc, callback, Output, Input, State
+import dash_bootstrap_components as dbc
+from dash_bootstrap_components._components.Container import Container
+import strategy_selector
+
+#instantiate web page
+dash.register_page(__name__, name='Backtest Strategies')
+
+#style right-sidebar for controls
+right_sidebar= {
+    "position": "fixed",
+    "top": 0,
+    "right": 0,
+    "bottom": 0,
+    "overflow": "auto"    
+}
+#setup page layout
+layout= html.Div([
+    dbc.Container([
+        dbc.Col([
+          dbc.Row([
+              dbc.Col(html.Div([html.Label('Input startDate'),dcc.Input(id = 'start-date', type='text',
+            minLength=10, maxLength=10, value = '2023-01-01',
+            placeholder = 'yyyy-mm-dd',style = {'border-radius':'50px', 'text-align':'center'})],
+            style = {'text-align':'center'})),
+             dbc.Col(html.Div([html.Label('Input endDate'),dcc.Input(id = 'end-date', type='text', minLength=10,maxLength=10,
+              value='2023-12-31', placeholder = 'yyyy-mm-dd',style ={'border-radius':'50px', 'text-align':'center'})],
+            style = {'text-align':'center'})),
+             dbc.Col(dbc.Button('Submit',id='date-inputerbutton',
+                n_clicks=0, style={'background-color':'black'}))
+          ], style={'marginBottom':'20px'}),
+          #prepare layout for selecting the trading strategies to backtest
+         dbc.Row([
+         html.Div([
+        html.H5('Select trading strategies to backtest(Cash=1000, Commission=0.1%)'),
+        dcc.Dropdown(id='backtester-dropdown', options=[
+        {'label':html.Pre('1. buy and hold when ema(50period) crosses above the middle bollinger\nsell when ema crosses below the middle bollinger.',
+                         style={'marginTop':'20px'}),'value':'EmaBband'},
+        {'label':html.Pre('2. buy if closingprice is above the middle bolinger and rsi>20 and 60\nsell if closingprice is in reverse and rsi < 80 and 60.',
+                         style={'marginTop':'20px'}), 'value':'BbandRsi'},
+        {'label':html.Pre('3. buy if shorter ema(10period) crosses above a longer ema(20period) and rsi(7period)>60\nsell if in reverse.',
+                         style={'marginTop':'20px'}), 'value':'EmaRsi'},
+        {'label':html.Pre('4. buy if shorter sma(10period) crosses above a longer sma(20period) and rsi(7period)>60\nsell if in reverse.',
+                         style={'marginTop':'20px'}), 'value':'RsiSma'},
+        {'label':html.Pre('5. buy if rsi(7periods) > 20 and 60\nsell if rsi < 80 and 60.',
+                         style={'marginTop':'20px'}), 'value':'SimpleRsi'},
+        {'label':html.Pre('6. buy if shorter ema(10period) crosses above a longer ema(20period)\nsell if in reverse.',
+                         style={'marginTop':'20px'}), 'value':'SimpleEma'},
+        {'label':html.Pre('7. buy if closingprice > sma(20period), sell if closingprice < sma.',
+                          style={'marginTop':'20px'}), 'value':'SmaPrice'}
+                 ])
+             ])    
+         ]),
+            dbc.Row([
+             html.Pre(id='backtester-output')
+         ])
+        ], width=9),
+        dbc.Col([
+            dbc.Navbar(
+                dbc.Nav([
+                    dbc.NavItem(
+          html.Div([
+              html.Label('Select Stock', style={'color':'#BF9B30'}),
+              dcc.Dropdown(id='ticker-dropdown', options=[
+            {'label':'MSFT', 'value':'MSFT'},
+            {'label':'AAPL', 'value':'AAPL'},
+            {'label':'META', 'value':'META'},
+            {'label':'AMZN', 'value':'AMZN'},
+            {'label':'TSLA', 'value':'TSLA'},
+            {'label':'GOOGL', 'value':'GOOGL'},
+            {'label':'WMT', 'value':'WMT'},
+            {'label':'NVDA', 'value':'NVDA'},
+            {'label':'AMZN', 'value':'AMZN'},
+            {'label':'ORCL', 'value':'ORCL'},
+            {'label':'JMIA', 'value':'JMIA'},
+            {'label':'IHS', 'value':'IHS'}
+              ], clearable=False, value='MSFT',
+                           style={'border-radius':'50px', 'width':'185px', 'background-color':'grey'})
+          ], style={'text-align':'center','font-weight':'bold'})  
+        )
+                ], vertical=True, navbar=True),
+            style=right_sidebar, color ='dark', dark = True)
+        ], width=3),
+    ], fluid=True),
+], style={"margin-top":"100px"})
+@callback(
+    Output(component_id='backtester-output', component_property='children'),
+    Input(component_id='ticker-dropdown', component_property='value'),
+    Input(component_id='date-inputerbutton', component_property='n_clicks'),
+    State(component_id='start-date', component_property='value'),
+    State(component_id='end-date', component_property='value'),
+    Input(component_id='backtester-dropdown', component_property='value')
+)
+def run_backtester(ticker, clicks, start_date, end_date, strategy):
+    ss= strategy_selector
+    if clicks is None:
+        start_date = None
+        end_date = None
+    else:
+        start_date = start_date
+        end_date = end_date
+    #run the backtester algorithm
+    if strategy == 'EmaBband':
+        return ss.bband_ema_strategy(ticker=ticker, start_date=start_date, end_date=end_date)
+    elif strategy == 'BbandRsi':
+        return ss.bband_rsi_strategy(ticker=ticker, start_date=start_date, end_date=end_date)
+    elif strategy == 'EmaRsi':
+        return ss.ema_rsi_strategy(ticker=ticker, start_date=start_date, end_date=end_date)
+    elif strategy == 'RsiSma':
+        return ss.rsi_sma_strategy(ticker=ticker, start_date=start_date, end_date=end_date)
+    elif strategy == 'SimpleRsi':
+        return ss.rsi_strategy(ticker=ticker, start_date=start_date, end_date=end_date)
+    elif strategy == 'SimpleEma':
+        return ss.ema_strategy(ticker=ticker, start_date=start_date, end_date=end_date)
+    elif strategy == 'SmaPrice':
+        return ss.sma_closeprice_strategy(ticker=ticker, start_date=start_date, end_date=end_date)
